69caa1427569b14e325ab7b04ab47eb3c7273ea1
travelgeezer <travelgeezer@icloud.com>
2017-11-07 00:11:56 +0800

    [T] containers: create App、Layout、Home component

---
+++ src/containers/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './home.less';
+
+class Home extends Component {
+  render() {
+    return (
+      <div class="home">
+        <p>This is home page</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
