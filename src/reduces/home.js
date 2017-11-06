4d6c7738956ecbd192a473bb047bfb00ce26f322
travelgeezer <travelgeezer@icloud.com>
2017-11-07 00:10:09 +0800

    [T] reduces: initial reduces && add home reduce

---
+++ src/reduces/home.js
@@ -0,0 +1,28 @@
+import { types } from '../actions/home';
+
+const initialState = {};
+
+export default function home( state = initialState, action = {}) {
+  const { payload } = action;
+
+  switch (action.type) {
+
+  case types.GET_HOME_INFO:
+    return {
+      ...state
+    };
+
+  case types.GET_HOME_INFO_SUCCESS:
+    return {
+      ...state
+    };
+
+  case types.GET_HOME_INFO_FAIL:
+    return {
+      ...state
+    };
+
+  default:
+    return state;
+  }
+}
---
+++ src/reduces/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import home from './home';
+
+const rootReduces = combineReducers({
+  home
+});
+
+export default rootReduces;
