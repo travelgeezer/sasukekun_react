1ef6b31234f02a8ce85cba7d8d5bdeb51b7b55cb
travelgeezer <travelgeezer@icloud.com>
2017-11-07 00:07:55 +0800

    [T] client: try use react-router

--- src/client.js
+++ src/client.js
@@ -1,10 +1,28 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
-import './test.less';
+import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { useBasename } from 'history';
+import createRoutes from './routes';
+import configureStore from './store/configure-store';
+import rootSaga from './sagas/index';
+
+const store = configureStore();
+store.runSaga(rootSaga);
+
+const routes = createRoutes(store);
+let history = browserHistory;
+console.log(browserHistory)
+if (process.env.NODE_ENV !== 'development') {
+  history = useBasename(() => browserHistory)({
+    basename: '/'
+  });
+}
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store} key="provider">
+    <Router routes={routes} history={history} />
+  </Provider>,
   document.getElementById('main')
 )
