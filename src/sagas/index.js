d62d7a572bd3c78a19a091b88955cf1d72d8c7ab
travelgeezer <travelgeezer@icloud.com>
2017-11-07 16:46:58 +0800

    [T] sagas: add blog

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,13 @@
 import { fork, all } from 'redux-saga/effects';
 
 import * as home from './home';
+import * as blog from './blog';
 
 export default function* rootSaga() {
   yield all([
     fork(home.watchGetHomeInfo),
+
+    fork(blog.watchCreateBlog),
+    fork(blog.watchGetBlogList)
   ]);
 }
