33af8fba8ff7651e53faa27450378e7eb2c70af5
travelgeezer <travelgeezer@icloud.com>
2017-11-07 15:49:04 +0800

    [T] client: add storage name space

--- src/client.js
+++ src/client.js
@@ -2,16 +2,17 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory'
 import { Provider } from 'react-redux';
 import { App } from './containers';
 import configureStore from './store/configure-store';
 import rootSaga from './sagas/index';
+import Storage from './utils/Storage';
+import configs from './configs';
 
 const store = configureStore();
 store.runSaga(rootSaga);
 
-const history = createBrowserHistory()
+Storage.setNamespace(configs.storageNameSpace);
 
 ReactDOM.render(
   <Provider store={store} key="provider">
