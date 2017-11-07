e04880a70baa116e2fdaf0f57ae46d0d8f7a26df
travelgeezer <travelgeezer@icloud.com>
2017-11-07 13:50:30 +0800

    [T] style: remove the redundant code

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,15 +3,11 @@ import { Route } from 'react-router-dom';
 import Layout from '../Layout';
 import Home from '../Home';
 
-
-import App3 from '../../App';
-
 class App extends React.Component {
   render() {
     return (
       <Layout>
         <Route path="/" component={Home} />
-        <Route path="/app" component={App3} />
       </Layout>
     );
   }
