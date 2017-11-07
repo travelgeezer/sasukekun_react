7fd49129cc7c58128b2cbf3834b0f73c4b115d5c
travelgeezer <travelgeezer@icloud.com>
2017-11-07 16:48:26 +0800

    [T] reduces: add blog

---
+++ src/reduces/blog.js
@@ -0,0 +1,36 @@
+import { types } from '../actions/blog';
+
+const initialState = {};
+
+export default function blog(state = initialState, action = {}) {
+  const { payload } = action;
+
+  switch (action.type) {
+  case types.CREATE_BLOG:
+    return {
+      ...state
+    };
+  case types.CREATE_BLOG_SUCCESS:
+    return {
+      ...state
+    };
+  case types.CREATE_BLOG_FAIL:
+    return {
+      ...state
+    };
+  case types.GET_BLOG_LIST:
+    return {
+      ...state
+    };
+  case types.GET_BLOG_LIST_SUCCESS:
+    return {
+      ...state
+    };
+  case types.GET_BLOG_LIST_FAIL:
+    return {
+      ...state
+    };
+  default:
+    return state;
+  }
+}
