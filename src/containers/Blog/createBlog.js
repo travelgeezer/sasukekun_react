63803ad82a65ebadcd7deac213e0964e32d2ce8d
travelgeezer <travelgeezer@icloud.com>
2017-11-08 01:09:36 +0800

    [T] blog: add create functional

--- src/containers/Blog/createBlog.js
+++ src/containers/Blog/createBlog.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Form, Input, Button } from 'antd';
 import * as blogActions from '../../actions/blog';
+
+const FormItem = Form.Item;
+
+const formItemLayout = {
+  labelCol: { span: 3 },
+  wrapperCol: { span: 10 }
+};
 
 class CreateBlog extends Component {
 
@@ -10,18 +18,85 @@ class CreateBlog extends Component {
     this.state = {};
   }
 
+  onCreateBlog(e) {
+    e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+      if (!err) {
+        values.slug = values.title;
+        values.tags = [values.tags];
+        this.props.blogActions.createBlog({data: values});
+      }
+    });
+
+  }
+
   render() {
-    console.log('create blog:');
-    console.log(this.props);
+    const { getFieldDecorator } = this.props.form;
     return (
       <div>
         <h1>create blog.</h1>
-        <p>{JSON.stringify(this.props)}</p>
-      </div>
+        <Form onSubmit={this.onCreateBlog.bind(this)}>
+          <FormItem
+            label="title"
+            {...formItemLayout}
+            >
+            {getFieldDecorator('title', {
+              rules: [{required: true}]
+            })(<Input placeholder="blog title" />)}
+      </FormItem>
+        <FormItem
+      label="abstract"
+      {...formItemLayout}
+        >
+        {getFieldDecorator('abstract', {
+          rules: [{required: true}]
+        })(<Input placeholder="blog abstract" />)}
+      </FormItem>
+        <FormItem
+      label="raw"
+      {...formItemLayout}
+        >
+        {getFieldDecorator('raw', {
+          rules: [{required: true}]
+        })(<Input placeholder="blog raw" />)}
+      </FormItem>
+        <FormItem
+      label="author"
+      {...formItemLayout}
+        >
+        {getFieldDecorator('author', {
+          rules: [{required: true}]
+        })(<Input placeholder="blog author" />)}
+
+      </FormItem>
+        <FormItem
+      label="category"
+      {...formItemLayout}
+        >
+        {getFieldDecorator('category', {
+          rules: [{required: true}]
+        })(<Input placeholder="blog category" />)}
+
+      </FormItem>
+        <FormItem
+      label="tags"
+      {...formItemLayout}
+        >
+        {getFieldDecorator('tags', {
+          rules: [{required: true}]
+        })(<Input placeholder="blog tags" />)}
+
+      </FormItem>
+        <FormItem>
+        <Button type="primary" htmlType="submit">save</Button>
+      </FormItem>
+      </Form>
+        </div>
     );
   }
 }
 
+CreateBlog = Form.create()(CreateBlog);
 
 function mapStateToProps(state) {
   return {
@@ -34,6 +109,7 @@ function mapDispatchToProps(dispatch) {
     blogActions: bindActionCreators(blogActions, dispatch)
   };
 }
+
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateBlog);
