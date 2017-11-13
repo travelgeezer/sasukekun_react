f551e909bea4c1ccbdd70d4e94a3170cfa220990
travelgeezer <travelgeezer@icloud.com>
2017-11-13 15:41:05 +0800

    [T] upload: update style of fileUpload

--- src/containers/upload/fileUpload.js
+++ src/containers/upload/fileUpload.js
@@ -1,8 +1,53 @@
 import React, { Component } from 'react';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import { formatLocalImageUrl } from '../../utils';
+import './styles.less';
+
+import FontIcon from 'material-ui/FontIcon';
+import {red500, yellow500, blue500} from 'material-ui/styles/colors';
+
+const iconStyles = {
+  alignItems: 'center'
+};
+
+const styles = {
+  uploadButton: {
+    verticalAlign: 'middle',
+  },
+  uploadInput: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  },
+}
+
 
 class FileUpload extends Component {
   render() {
-    return <div><p>upload file view</p></div>
+    return (
+      <div
+        className="file-upload"
+        style={{height: 464, display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+        >
+        <Card
+          style={{width: 230, height: 360, display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+          >
+          <FlatButton
+            label="upload file"
+            labelPosition="before"
+            style={styles.uploadButton}
+            containerElement="label"
+            >
+            <input type="file" style={styles.uploadInput} />
+          </FlatButton>
+        </Card>
+      </div>);
   }
 }
 
