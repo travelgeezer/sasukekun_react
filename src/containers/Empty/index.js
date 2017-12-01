2866bdec305ee30aa9183faa54fa0291dda0840f
travelgeezer <travelgeezer@icloud.com>
2017-12-01 16:14:42 +0800

    Added EmptyView

---
+++ src/containers/Empty/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EmptyView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>EmptyView</h1>
+      </div>
+    );
+  }
+}
+
+export default EmptyView;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as App } from './App';
 export { default as Layout } from './Layout';
 export { default as Home } from './Home';
+export { default as Empty} from './Empty';
